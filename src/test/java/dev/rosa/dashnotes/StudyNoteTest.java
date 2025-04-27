d52e599e12c6be26596926ec53240e0b6d34fa08
Rosa <angelica-2025@outlook.es>
2025-04-27 16:50:19 +0200

    First test of created StudyNote entity correctly failed

--- src/test/java/dev/rosa/dashnotes/StudyNoteTest.java
+++ src/test/java/dev/rosa/dashnotes/StudyNoteTest.java
@@ -1,5 +1,22 @@
 package dev.rosa.dashnotes;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+@DisplayName("Test for StudyNote entity")
 public class StudyNoteTest {
+
+    @Test
+    @DisplayName("Should create a StudyNote conrrectly"
+    public void studyNoteCreated(){
+        
+       StudyNote studyNote = new StudyNote ("CSS", "JAVA", "/files/git-rebase.pdf");
+        assertThat(studyNote.getTitle(), is("CSS"));
+        assertThat(studyNote.getDescription(), is("JAVA"));
+        assertThat(studyNote.getFilePath(), is("/files/git-rebase.pdf"));
+        assertThat(studyNote.getCreatedAt(), is(notNullValue()));
+    }
 
 }
